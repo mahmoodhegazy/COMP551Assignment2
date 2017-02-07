d0d53e7d951db040fce2d1aa381c9036eb1e53b9
Arun Rawlani <arun.rawlani@mail.mcgill.ca>
2017-02-06 22:59:58 -0500

    Added code for naive bayes using SciKit for testing. Will replace with complete implementation

--- CleanUpCode.py
+++ CleanUpCode.py
@@ -6,43 +6,47 @@
 import pandas as pd
 import numpy as np
 from pandas import DataFrame, Series
-
-left = pd.read_csv('/Users/praynaa/Downloads/train_input.csv')
-right = pd.read_csv('/Users/praynaa/Downloads/train_output.csv')
-
-frames = [left, right]
-joined = pd.concat(frames, axis=1)
-joined.shape
-
-
-# In[4]:
-
-category = np.where(joined['category'].str.contains('new'), 'Yes', 'No')
-
-
-# In[5]:
-
-category[:10]
-
-
-# In[7]:
-
-#or equivalently:
-#j = pd.merge(left, right) # merges the two datasets
-
-joined['conversation'][0]
-
-
-# In[ ]:
-
 import re
 import nltk
 nltk.download('stopwords') #contains the list of irrelevant words
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-from __future__ import print_function, unicode_literals
+
+left = pd.read_csv('/Users/praynaa/Downloads/train_input.csv')
+right = pd.read_csv('/Users/praynaa/Downloads/train_output.csv')
+
+
+frames = [left, right]
+joined = pd.concat(frames, axis=1)
+
+
+category = np.where(joined['category'].str.contains('new'), 'Yes', 'No')
+
 
 corpus = []
+i = 0
+
+def convert(x):
+    if (x == 'hockey'):
+        return 0
+    elif (x == 'movies'):
+        return 1
+    elif (x == 'nba'):
+        return 2
+    elif (x == 'news'):
+        return 3
+    elif (x == 'nfl'):
+        return 4
+    elif (x == 'politics'):
+        return 5
+    elif (x == 'soccer'):
+        return 6
+    elif (x == 'worldnews'):
+        return 7
+    else:
+        return 999999
+
+right['category'] = right.category.apply(convert)
 
 #Cleaning the texts
 for i in range(0,165000):
@@ -57,18 +61,31 @@ for i in range(0,165000):
     conversation = [stemmer.stem(word) for word in conversation if not word in set(stopwords.words('english'))] #removing all words in the stop word list
     conversation = ' '.join(conversation)
     
+    i = i+1
+    if(i%100 == 0):
+        print(i)
     corpus.append(conversation)
 
-
-#Creating Bag Of Words model
 from sklearn.feature_extraction.text import CountVectorizer
-cv = CountVectorizer(max_features=300) #Chooses the top 1500 features
+cv = CountVectorizer(max_features=1500) #Chooses the top 1500 features
 X = cv.fit_transform(corpus).toarray()
 
-Y = right.iloc[:,1].values
+y = right.iloc[:,1].values
 
 
-# In[ ]:
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
 
+#Fitting Naive Bayes to Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
 
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
