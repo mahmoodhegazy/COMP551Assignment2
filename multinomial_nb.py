c005040bd21d90c852d905a0a69db1e27356fee3
Arun Rawlani <arun.rawlani@mail.mcgill.ca>
2017-02-07 16:22:54 -0500

    Pushing the code for multinomial naive bayes.

--- multinomial_nb.py
+++ multinomial_nb.py
@@ -1,5 +1,5 @@
 from sklearn.feature_extraction.text import CountVectorizer
-cv = CountVectorizer(analyzer='word', ngram_range=(1,3), max_features=1500) #Chooses the top 1500 features
+cv = CountVectorizer(lowercase=True, analyzer='word', ngram_range=(1,3), max_features=1500) #Chooses the top 1500 features
 X = cv.fit_transform(corpus).toarray()
 
 y = right.iloc[:,1].values
@@ -10,8 +10,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, rand
 
 
 #Fitting Naive Bayes to Training set
-from sklearn.naive_bayes import GaussianNB
-classifier = GaussianNB()
+from sklearn.naive_bayes import MultinomialNB
+classifier = MultinomialNB(alpha=1.0, class_prior=None, fit_prior=True)
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
@@ -19,6 +19,7 @@ y_pred = classifier.predict(X_test)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
-cmnb = confusion_matrix(y_test, y_pred)
+cm = confusion_matrix(y_test, y_pred)
 
+print('Score: ')
 print(classifier.score(X_test, y_test))
