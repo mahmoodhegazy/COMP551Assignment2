f6809f77ee6e238cd10b969fd24b22374106f0b4
mahmoodhegazy <mahmoudhegazya@gmail.com>
2017-02-08 21:05:19 -0500

    Decision trees should be working now

--- SKLearn_Algorithms/decision_trees.py
+++ SKLearn_Algorithms/decision_trees.py
@@ -6,12 +6,13 @@ import numpy as np
 import pandas as pd
 
 def decision_trees(X, Y):
-	cv = CountVectorizer(lowercase=True, analyzer='word', ngram_range=(1,3), max_features=1500)
-	word_counter = cv.fit_transform(X)
-	X_train, X_test, Y_train, Y_test = train_test_split(word_counter, Y, test_size = 0.30, random_state = 0)
-	clf = tree.DecisionTreeClassifier()
-	clf = clf.fit(X_train,Y_train)
-	Y_pred = clf.predict(X_test)
-	confusion_matrix = confusion_matrix(Y_test, Y_pred)
-	print('Score: ')
-	print(clf.score(X_test, Y_test))
+    confusion_mat = 1
+    cv = CountVectorizer(lowercase=True, analyzer='word', ngram_range=(1,3), max_features=1500)
+    word_counter = cv.fit_transform(X)
+    X_train, X_test, Y_train, Y_test = train_test_split(word_counter, Y, test_size = 0.30, random_state = 0)
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(X_train,Y_train)
+    Y_pred = clf.predict(X_test)
+    confusion_mat = confusion_matrix(Y_test, Y_pred)
+    print('Score: ')
+    print(clf.score(X_test, Y_test))
--- sanitize_data.py
+++ sanitize_data.py
@@ -8,19 +8,17 @@ from utilities import Utilities
 util = Utilities()
 [training_set_X, training_set_Y] = util.read_input_data()
 
-X = []
 Y = training_set_Y[0:100]
 
 def sanitize_data():
-	for i in range(0,100):
-		conversation_with_tags = training_set_X[i]
-		conversation = util.remove_tags(conversation_with_tags)
-		conversation = util.remove_punctuation(conversation) 
-		conversation = conversation.lower().split() #sets everything to lowercase and splits on the spaces by default
-		#conversation = util.stem(conversation) #stemming (taking the root of the word)
-		conversation = util.lemmatize(conversation)
-		X.append(conversation)
-	X = np.array(X)
-	Y = np.array(Y)
-	return X, Y
+    X = []
+    for i in range(0,100):
+        conversation_with_tags = training_set_X[i]
+        conversation = util.remove_tags(conversation_with_tags)
+        conversation = util.remove_punctuation(conversation) 
+        conversation = conversation.lower().split() #sets everything to lowercase and splits on the spaces by default
+        #conversation = util.stem(conversation) #stemming (taking the root of the word)
+        conversation = util.lemmatize(conversation)
+        X.append(conversation)
+    return X,Y
 
