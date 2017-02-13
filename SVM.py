056b4a7a39c0f155bc285143df2fe2272987c034
mahmoodhegazy <mahmoudhegazya@gmail.com>
2017-02-13 18:49:05 -0500

    fixed some typos

--- SVM.py
+++ SVM.py
@@ -16,10 +16,11 @@ from sklearn.feature_extraction.text import TfidfTransformer
 
 
 cv = CountVectorizer(lowercase=True, analyzer='word', ngram_range=(1,3),max_features=2836998)  #max out the number of features constructed to the number of features the kaggle input can construct 
-df_full_input = pd.read_csv('cleaned_data/full_space.csv')
+df_full_input = pd.read_csv('cleaned_data/full_input_space_forcv.csv')
+df_clean = pd.read_csv('train_data/full_sanitized_data.csv') #clean trainig data set
 word_counter = cv.fit_transform(df_full_input.clean_sentence.tolist()) # sparse input (for both training and testing data)
-wrod_counter_input_train = word_counter[0:165000] #take all non-kaggle data
 word_counter_input_test = word_counter[165000:218218] # last 53218 entries are for kaggle testing
+word_counter_input_train = word_counter[0:165000] #take all non-kaggle data for training/testing split
 # cross validations : 80/20 train/test split
 X_train, X_test, Y_train, Y_test = train_test_split(word_counter_input_train, df_clean.category, test_size = 0.20, random_state = 101)
 model = SVC(C=10,gamma=0.001,kernel='rbf')  #using best parameters found by grid search 
